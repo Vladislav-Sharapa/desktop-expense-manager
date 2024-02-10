f1dc02f86401ba97d28be4cd7179defcd7ba114f
Влад Шарапа <vl.sharapa20@gmail.com>
2024-02-11 00:33:31 +0300

    Session handler

--- src/database/session.py
+++ src/database/session.py
@@ -1,4 +1,3 @@
-from contextlib import contextmanager
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from .config import settings
@@ -9,14 +8,51 @@ engine = create_engine(
 ) 
 Session = sessionmaker(bind=engine)
 
-@contextmanager
-def session_scope():
-    session = Session()  # def __enter__
-    try:
-        yield session  #Pass session with as
-        session.commit()  #If nothing happens, commit()
-    except:
-        session.rollback()  #Rollback if error occurs()
-        raise
-    finally:
-        session.close()  #Either way, it will eventually close()
+
+def get_session():
+    with Session() as session:
+        yield session
+
+# @contextmanager
+# def ssession_scope():
+#     session = Session()  # def __enter__
+#     try:
+#         yield session  #Pass session with as
+#         session.commit()  #If nothing happens, commit()
+#     except:
+#         session.rollback()  #Rollback if error occurs()
+#         raise
+#     finally:
+#         session.close()  #Either way, it will eventually close()
+
+
+# def session_scope(func):
+#     def wrapper(self, *args, **kwargs):
+#         session = Session()
+#         try:
+#             result = func(self, session, *args, **kwargs)
+#             session.commit()
+#             return result
+#         except Exception as e:
+#             session.rollback()
+#             raise
+#         finally:
+#             session.close()
+#     return wrapper
+
+def session_scope(commit_required: bool = True):
+    def decorator(func):
+        def wrapper(self, *args, **kwargs):
+            session = Session()
+            try:
+                result = func(self, session, *args, **kwargs)
+                if commit_required:
+                    session.commit()
+                return result
+            except Exception as e:
+                session.rollback()
+                raise
+            finally:
+                session.close()
+        return wrapper
+    return decorator
